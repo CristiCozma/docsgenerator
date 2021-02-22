f8bc26cc3678c33a253bc5691a0a0628e795b478
Cozma Cristian <cozmacristian@HUAWEI_P9_lite_mini.localdomain>
2021-02-22 21:30:41 +0200

    improved search function

--- functions.js
+++ functions.js
@@ -9,7 +9,6 @@ const API = {
     }
 };
 
-console.log("test script");
 
 function insertPersons(persons) {
     const tbody = document.querySelector('#list tbody');
@@ -42,23 +41,55 @@ function loadList() {
 
 loadList();
 
+fetch(API.READ.URL + "/person?cnp=2901112265544")
 
 
 function searchPersons(cnp) {
     console.warn("search", cnp, allPersons);
-    const found = allPersons.find(function (p) {
-        return p.cnp === cnp;
+    return allPersons.filter(function (p) {
+        return p.cnp.indexOf(cnp) > -1;
     });
-    return found ? [found] : allPersons; //ternary if
+}
+
+
+function addPerson() {
+    const cnp = document.querySelector("input[name=cnp]").value;
+    const firstName = document.querySelector("input[name=firstName]").value;
+    const lastName = document.querySelector("input[name=lastName]").value;
+
+    const person = {
+        cnp,
+        firstName,
+        lastName
+    };
+    console.info("Saving...", person, JSON.stringify(person));
+
+    fetch(API.CREATE.URL, {
+        method: API.CREATE.METHOD,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: API.CREATE.METHOD === "GET" ? null : JSON.stringify(person)
+    })
+        .then(res => res.json())
+        .then(r => {
+            if (r.success) {
+                loadList();
+            }
+        });
 }
 
 function addEventListner() {
-    const search = document.querySelector('#search');
+    const search = document.getElementById('search');
     search.addEventListener("input", e => {
         const cnp = e.target.value;
         const foundPerson = searchPersons(cnp);
         insertPersons(foundPerson);
     });
+
+    const saveBtn = document.querySelector('#list tfoot button');
+    saveBtn.addEventListener("click", addPerson);
 }
 
 addEventListner();
+
